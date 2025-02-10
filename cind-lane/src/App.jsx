766664df5fc1a49f811329fc854e8ef339564591
David <davidshuschool@gmail.com>
2025-02-10 14:31:28 -0600

    Initial Commit

---
+++ cind-lane/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import cindLaneLogo from '/cind_lane_logo.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={cindLaneLogo} class="" />
+        <span class="text-teal-600"> asdf</span>
+      </div>
+    </>
+  )
+}
+
+export default App
