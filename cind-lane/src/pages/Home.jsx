c50e23f4677daad5a570b18cc3ca825255299cb5
David <davidshuschool@gmail.com>
2025-02-23 14:54:25 -0600

    Made a basic homepage

--- cind-lane/src/pages/Home.jsx
+++ cind-lane/src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import cindLaneLogo from '/cind_lane_logo.png';
+
 function Home() {
     return (
-        <div>
-            <p>Home</p>
+        <div className="h-full flex items-center justify-center overflow-hidden">
+            <div className="w-1/2 flex flex-col items-center justify-center h-full bg-gray-200 p-8">
+                <div className="w-full flex flex-col items-center text-center">
+                    <img src={cindLaneLogo} alt="CindLane Logo" className="max-w-[50%] h-auto" />
+                    <h1 className='text-4xl font-bold text-primary mt-4'>
+                        Get insights on your Pokemon Unite Team
+                    </h1>
+                    <h2 className='text-2xl text-primary mt-2'>
+                        100% automatically and free
+                    </h2>
+                </div>
+            </div>
+
+            <div className="w-1/2 flex justify-center">
+                <img src={cindLaneLogo} alt="CindLane Logo" className="max-w-[50%] h-auto" />
+            </div>
         </div>
-    )
+    );
 }
 
 export default Home;
