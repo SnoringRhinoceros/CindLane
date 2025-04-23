87da5f905b7eb33c0bd9b0412dcae230838628cd
David <davidshuschool@gmail.com>
2025-04-23 14:31:58 -0500

    Fixed incorrect import x2

--- cind-lane/src/components/BestPokemonBox.jsx
+++ cind-lane/src/components/BestPokemonBox.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import TooltipPortal from "../portals/ToolTipPortal";
+import TooltipPortal from "../portals/TooltipPortal";
 import StatFilterCheckbox from "./StatFilterCheckbox";
 
 function BestPokemonBox({
